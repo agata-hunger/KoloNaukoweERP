c3118368a12b85add74d7b26953a9a6bcdc7eb65
agata-hunger <agatah2000@o2.pl>
2023-04-06 21:58:27 +0200

    fix bugs
    # delete old WebAPI project
    # move connectionString from appsettings.Development.json to appsettings.json
    # add some methods that are needed in Program.cs
    # change launchUrl in launchSettings.json
    # fix bug that was received during database update
    # add a first controller - CzlonkowieController

--- KoloNaukoweERP/WebAPIKN/Program.cs
+++ KoloNaukoweERP/WebAPIKN/Program.cs
@@ -5,8 +5,8 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllersWithViews();
 
-builder.Services.AddControllers();
 builder.Services.AddDbContext<DbKoloNaukoweERP>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -15,14 +15,22 @@ builder.Services.AddEndpointsApiExplorer();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (!app.Environment.IsDevelopment())
 {
-    //app.UseSwagger();
-    //app.UseSwaggerUI();
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
 }
+
+app.UseStaticFiles();
+/*app.UseMiddleware<ErrorHandlingMiddleware>();*/
+app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllers();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+//app.MapControllers();
