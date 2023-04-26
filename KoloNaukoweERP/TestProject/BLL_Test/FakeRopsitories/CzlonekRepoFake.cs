d0b0f1762b9ab2bbdddcbb6b6bebba39066d3ff7
agata-hunger <agatah2000@o2.pl>
2023-04-26 22:50:12 +0200

    ProjektRepoFake, CzlonekRepoFake
    # add new fake repos

---
+++ KoloNaukoweERP/TestProject/BLL_Test/FakeRopsitories/CzlonekRepoFake.cs
@@ -0,0 +1,40 @@
+﻿using DAL.Entities;
+using DAL.Repositories.CzlonekR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject.BLL_Test.FakeRopsitories
+{
+    public class CzlonekRepoFake : ICzlonekRepository
+    {
+        private List<Czlonek> czlonkowie = new List<Czlonek>();
+
+
+        public IEnumerable<Czlonek> GetCzlonkowie()
+        {
+            return czlonkowie;
+        }
+        public Czlonek GetCzlonekById(int idCzlonka)
+        {
+            return czlonkowie.Find(cz => cz.IdCzlonka == idCzlonka);
+        }
+        public void InsertCzlonek(Czlonek czlonek)
+        {
+            czlonkowie.Add(czlonek);
+        }
+        public void DeleteCzlonek(int idCzlonka)
+        {
+            Czlonek czlonek = czlonkowie.Find(cz => cz.IdCzlonka == idCzlonka);
+            czlonkowie.Remove(czlonek);
+        }
+        public void UpdateCzlonek(Czlonek czlonek)
+        {
+            int index = czlonkowie.FindIndex(cz => cz.IdCzlonka == czlonek.IdCzlonka);
+            if (index != -1)
+                czlonkowie[index] = czlonek;
+        }
+    }
+}
---
+++ KoloNaukoweERP/TestProject/BLL_Test/FakeRopsitories/ProjektRepoFake.cs
@@ -0,0 +1,41 @@
+﻿using DAL.Entities;
+using DAL.Repositories.ProjektR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject.BLL_Test.FakeRopsitories
+{
+    public class ProjektRepoFake : IProjektRepository
+    {
+        private List<Projekt> projekty = new List<Projekt>();
+
+
+        public IEnumerable<Projekt> GetProjekty()
+        {
+            return projekty;
+        }
+        public Projekt GetProjektById(int idProjektu)
+        {
+            return projekty.Find(p => p.IdProjektu == idProjektu);
+
+        }
+        public void InsertProjekt(Projekt projekt)
+        {
+            projekty.Add(projekt);
+        }
+        public void DeleteProjekt(int idProjektu)
+        {
+            Projekt projekt = projekty.Find(p => p.IdProjektu == idProjektu);
+            projekty.Remove(projekt);
+        }
+        public void UpdateProjekt(Projekt projekt)
+        {
+            int index = projekty.FindIndex(p => p.IdProjektu == projekt.IdProjektu);
+            if (index != -1)
+                projekty[index] = projekt;
+        }
+    }
+}
