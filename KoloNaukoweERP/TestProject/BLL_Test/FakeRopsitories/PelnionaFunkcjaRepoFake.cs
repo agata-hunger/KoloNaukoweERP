ec01a0de7ed285f670d5479479a32dff56e06dab
milomalecki <milomal486@student.polsl.pl>
2023-04-26 22:34:35 +0200

    Dodanie Fake Repo
    - ZespolRepoFake
    - PelnionaFunkcjaRepoFake

---
+++ KoloNaukoweERP/TestProject/BLL_Test/FakeRopsitories/PelnionaFunkcjaRepoFake.cs
@@ -0,0 +1,40 @@
+﻿using DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject.BLL_Test.FakeRopsitories
+{
+    public class PelnionaFunkcjaRepoFake
+    {
+        private List<PelnionaFunkcja> pelnioneFunkcje = new List<PelnionaFunkcja>();
+
+
+        public IEnumerable<PelnionaFunkcja> GetPelnioneFunkcje()
+        {
+            return pelnioneFunkcje;
+        }
+        public PelnionaFunkcja GetPelnionaFunkcjaById(int idPelnioneFunkcje)
+        {
+            return pelnioneFunkcje.Find(p => p.IdPelnionejFunkcji == idPelnioneFunkcje);
+
+        }
+        public void InsertPelnionaFunkcja(PelnionaFunkcja pelnionaFunkcja)
+        {
+            pelnioneFunkcje.Add(pelnionaFunkcja);
+        }
+        public void DeletePelnionaFunkcja(int idPelnionejFunkcji)
+        {
+            PelnionaFunkcja pelnionaFunkcja = pelnioneFunkcje.Find(p => p.IdPelnionejFunkcji == idPelnionejFunkcji);
+            pelnioneFunkcje.Remove(pelnionaFunkcja);
+        }
+        public void UpdatePelnionaFunkcja(PelnionaFunkcja pelnionaFunkcja)
+        {
+            int index = pelnioneFunkcje.FindIndex(p => p.IdPelnionejFunkcji == pelnionaFunkcja.IdPelnionejFunkcji);
+            if (index != -1)
+                pelnioneFunkcje[index] = pelnionaFunkcja;
+        }
+    }
+}
---
+++ KoloNaukoweERP/TestProject/BLL_Test/FakeRopsitories/SprzetRepoFake.cs
@@ -0,0 +1,40 @@
+﻿using DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject.BLL_Test.FakeRopsitories
+{
+    public class SprzetRepoFake
+    {
+        private List<Sprzet> sprzety = new List<Sprzet>();
+
+
+        public IEnumerable<Sprzet> GetSprzet()
+        {
+            return sprzety;
+        }
+        public Sprzet GetSprzetById(int idSprzety)
+        {
+            return sprzety.Find(s => s.IdSprzetu == idSprzety);
+
+        }
+        public void InsertSprzet(Sprzet sprzet)
+        {
+            sprzety.Add(sprzet);
+        }
+        public void DeleteSprzet(int idSprzetu)
+        {
+            Sprzet sprzet = sprzety.Find(s => s.IdSprzetu == idSprzetu);
+            sprzety.Remove(sprzet);
+        }
+        public void UpdateSprzet(Sprzet sprzet)
+        {
+            int index = sprzety.FindIndex(s => s.IdSprzetu == sprzet.IdSprzetu);
+            if (index != -1)
+                sprzety[index] = sprzet;
+        }
+    }
+}
