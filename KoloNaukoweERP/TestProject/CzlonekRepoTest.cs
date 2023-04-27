a73359a701002ac3feec5c522b4e530eeaa1a436
Daniel Malik <danimal889@student.polsl.pl>
2023-04-27 12:30:59 +0200

    test repo czlonek

---
+++ KoloNaukoweERP/TestProject/CzlonekRepoTest.cs
@@ -0,0 +1,39 @@
+﻿using DAL;
+using DAL.Entities;
+using DAL.Repositories.CzlonekR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    public class CzlonekRepoTest
+    {
+        [Fact]
+        public void TestGetCzlonkowie()
+        {
+            var options = new DbContextOptionsBuilder<DbKoloNaukoweERP>()
+                .UseInMemoryDatabase(databaseName: "Test")
+                .Options;
+
+            var context = new DbKoloNaukoweERP(options);
+
+            CzlonekRepository czlonekRepo = new CzlonekRepository(context);
+
+            var czlonek = new Czlonek { IdCzlonka = 1, Imie = "Adam" };
+
+            czlonekRepo.InsertCzlonek(czlonek);
+
+            czlonekRepo.Save();
+
+            Assert.Equal(1, czlonekRepo.GetCzlonkowie().Count());
+                
+        }
+
+
+
+    }
+}
