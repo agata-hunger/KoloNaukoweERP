f0f3e64a59bd3c8c0eb035006cc77984109e8695
milomalecki <milomal486@student.polsl.pl>
2023-05-15 23:19:36 +0200

    Dodanie testu moq

--- KoloNaukoweERP/TestProject/BLL_Test/WydarzenieServiceTest.cs
+++ KoloNaukoweERP/TestProject/BLL_Test/WydarzenieServiceTest.cs
@@ -28,7 +28,7 @@ namespace TestProject.BLL_Test
         private readonly Mock<IProjektRepository> mockProjektRepo = new Mock<IProjektRepository>();
         private readonly Mock<IPelnionaFunkcjaRepository> mockPelnionaFunkcjaRepo = new Mock<IPelnionaFunkcjaRepository>();
         private readonly Mock<ICzlonekRepository> mockCzlonekRepo = new Mock<ICzlonekRepository>();
-
+        
         [Fact]
         public void TestAddWydarzenie()//string nazwaWydarzenia, string nazwaZespolu, DateTime dataWydarzenia, string miejsceWydarzenia
         {
@@ -43,11 +43,25 @@ namespace TestProject.BLL_Test
             var unitOfWork = new UnitOfWork(czlonekRepo, pelnionaFunkcjaRepo, projektRepo, sprzetRepo, wydarzenieRepo, zespolRepo);
             var sekretarz = new SekretarzeServices(unitOfWork);
             sekretarz.AddWydarzenie("Test", "Test", DateTime.Now, "Test");
-
             Assert.Equal(1, wydarzenieRepo.GetWydarzenia().Count());
         }
+        [Fact]
+        public void TestAddWydarzenieMoq()
+        {
+            Mock<IWydarzenieRepository> mockWydarzenieRepo = new Mock<IWydarzenieRepository>();
+            Mock<IZespolRepository> mockZespolRepo = new Mock<IZespolRepository>();
+            Mock<ISprzetRepository> mockSprzetRepo = new Mock<ISprzetRepository>();
+            Mock<IProjektRepository> mockProjektRepo = new Mock<IProjektRepository>();
+            Mock<IPelnionaFunkcjaRepository> mockPelnionaFunkcjaRepo = new Mock<IPelnionaFunkcjaRepository>();
+            Mock<ICzlonekRepository> mockCzlonekRepo = new Mock<ICzlonekRepository>();
 
-        public void RemoveWydarzenie(string nazwaWydarzenia)
+            var unitOfWork = new UnitOfWork(mockCzlonekRepo.Object, mockPelnionaFunkcjaRepo.Object, mockProjektRepo.Object, mockSprzetRepo.Object, mockWydarzenieRepo.Object, mockZespolRepo.Object);
+            var sekretarzBLL = new SekretarzeServices(unitOfWork);
+
+            sekretarzBLL.AddWydarzenie("Test", "Test", DateTime.Now, "Test");
+            Assert.Equal(1, sekretarzBLL.GetEvents().Count());
+        }
+    public void RemoveWydarzenie(string nazwaWydarzenia)
         {
             throw new NotImplementedException();
         }
