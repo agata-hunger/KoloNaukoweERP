92809eaad7885f105c49493662e68b117ad7eebc
agata-hunger <agatah2000@o2.pl>
2023-05-16 19:54:25 +0200

    UnitTestWydarzenieController, UnitTestWydarzenieApiController fix
    #implement TestGetAction() in UnitTestWydarzenieController
    #fix failures TestGetAction() in UnitTestWydarzenieApiController

--- KoloNaukoweERP/TestControllersMVC/UnitTestWydarzenieController.cs
+++ KoloNaukoweERP/TestControllersMVC/UnitTestWydarzenieController.cs
@@ -1,38 +1,38 @@
-﻿using System;
+﻿using BLL.Services.Sekretarz;
+using DAL.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using BLL.Services.Sekretarz;
-using DAL.Entities;
-using Microsoft.AspNetCore.Mvc;
-using Moq;
 using WebAPIKN.Controllers;
 using WebAPIKN.Models;
 
 namespace TestControllersMVC
 {
-    public class UnitTestWydarzenieApiController
+    public class UnitTestWydarzenieController
     {
         [Fact]
         public void TestGetAction()
         {
-            Mock<ISekretarzServices> mockSekretarzService = new Mock<ISekretarzServices>();
+            Mock<ISekretarzServices> mockSekretarzServices = new Mock<ISekretarzServices>();
 
             var wydarzenia = new List<Wydarzenie>();
-
-            mockSekretarzService
+            
+            mockSekretarzServices
                 .Setup(w => w.GetEvents())
                 .Returns(wydarzenia);
 
-            WydarzeniaController wydarzeniaController = new WydarzeniaController(mockSekretarzService.Object);
-           
+            WydarzeniaController wydarzeniaController = new WydarzeniaController(mockSekretarzServices.Object);
+
             var result = wydarzeniaController.Get();
             var actionResult = Assert.IsType<ActionResult<List<WydarzenieDTO>>>(result);
             var viewResult = Assert.IsType<ViewResult>(actionResult.Result);
             var model = Assert.IsAssignableFrom<List<Wydarzenie>>(viewResult.Model);
 
-            Assert.Same(model, wydarzenia);
+            Assert.Same(viewResult.Model, wydarzenia);
         }
     }
 }
