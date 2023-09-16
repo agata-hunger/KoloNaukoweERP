8d5001e6cf3a060580dfc3e69f6d9e00d57a7f4b
agata-hunger <agatah2000@o2.pl>
2023-09-16 20:07:41 +0200

    WebAPI - Controllers
    # add new controller - KoordynatorController

---
+++ KoloNaukoweERP/WebAPIKN/Controllers/KoordynatorController.cs
@@ -0,0 +1,31 @@
+﻿using BLL.Models;
+using BLL.Services.Koordynator;
+using BLL.Services.Lider;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPIKN.Controllers
+{
+    public class KoordynatorController : Controller
+    {
+        public readonly IKoordynatorServices koordynatorServices;
+
+        public KoordynatorController(IKoordynatorServices koordynatorServices)
+        {
+            this.koordynatorServices = koordynatorServices;
+        }
+
+        [HttpPost]
+        public IActionResult AddWypozyczenie(int idCzlonka, SprzetDTO sprzetDto)
+        {
+            koordynatorServices.AddWypozyczenie(idCzlonka, sprzetDto);
+            return View();
+        }
+
+        [HttpDelete]
+        public IActionResult RemoveWypozyczenie(int idCzlonka, SprzetDTO sprzetDto)
+        {
+            koordynatorServices.RemoveWypozyczenie(idCzlonka, sprzetDto);
+            return View();
+        }
+    }
+}
