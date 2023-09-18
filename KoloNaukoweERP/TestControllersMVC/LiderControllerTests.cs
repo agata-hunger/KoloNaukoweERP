f21d9163a4b977793a932d2c18968604b3660837
agata-hunger <agatah2000@o2.pl>
2023-09-18 16:07:27 +0200

    TestControllersMVC - LiderControllerTests
    # prepare base for LiderControllerTests

---
+++ KoloNaukoweERP/TestControllersMVC/LiderControllerTests.cs
@@ -0,0 +1,88 @@
+﻿using BLL.Services.Lider;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPIKN.Controllers;
+
+namespace TestControllersMVC
+{
+    public class LiderControllerTests
+    {
+        [Fact]
+        public void AddWypozyczenieTest()
+        {
+            Mock<ILiderServices> mockLiderServices = new Mock<ILiderServices>();
+
+            //
+
+            var liderController = new LiderController(mockLiderServices.Object);
+
+            liderController.ControllerContext = new ControllerContext
+            {
+                //
+            };
+
+            //
+            //
+        }
+
+        [Fact]
+        public void RemoveWypozyczenieTest()
+        {
+            Mock<ILiderServices> mockLiderServices = new Mock<ILiderServices>();
+
+            //
+
+            var liderController = new LiderController(mockLiderServices.Object);
+
+            liderController.ControllerContext = new ControllerContext
+            {
+                //
+            };
+
+            //
+            //
+        }
+
+        [Fact]
+        public void AddZespolToProjectTest()
+        {
+            Mock<ILiderServices> mockLiderServices = new Mock<ILiderServices>();
+
+            //
+
+            var liderController = new LiderController(mockLiderServices.Object);
+
+            liderController.ControllerContext = new ControllerContext
+            {
+                //
+            };
+
+            //
+            //
+        }
+
+
+        [Fact]
+        public void RemoveZespolFromProjectTest()
+        {
+            Mock<ILiderServices> mockLiderServices = new Mock<ILiderServices>();
+
+            //
+
+            var liderController = new LiderController(mockLiderServices.Object);
+
+            liderController.ControllerContext = new ControllerContext
+            {
+                //
+            };
+
+            //
+            //
+        }
+    }
+}
