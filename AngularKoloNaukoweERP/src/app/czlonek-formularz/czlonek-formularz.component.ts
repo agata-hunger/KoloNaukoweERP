977440af2dc32dd6252bdad453511b8546507d3c
DanielMalik00 <danimal889@student.polsl.pl>
2023-09-18 17:04:45 +0200

    implements czlonek-formularz.component and czlonek-tabela.component

--- AngularKoloNaukoweERP/src/app/czlonek-formularz/czlonek-formularz.component.ts
+++ AngularKoloNaukoweERP/src/app/czlonek-formularz/czlonek-formularz.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { CzlonekResponse } from '../czlonek/models/czlonek-response';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { SekretarzService } from '../sekretarz.service';
+import { Router } from '@angular/router';
+import { CzlonekRequest } from '../czlonek/models/czlonek-request';
 
 @Component({
   selector: 'app-czlonek-formularz',
@@ -6,6 +11,58 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class CzlonekFormularzComponent {
+export class CzlonekFormularzComponent implements OnInit{
+
+  @Input() projekt?: CzlonekResponse = undefined;
+
+  formCzlonek!: FormGroup;
+
+
+              //upewnić się czy na pewno ten serwis!!!!!!!!!!!!!!!
+  constructor(private formBuilder: FormBuilder, private sekretarzService: SekretarzService, private router: Router) {
+
+  }
+
+  ngOnInit(): void {
+    this.formCzlonek = this.formBuilder.group({
+      idPelnionejFunkcji: this.formBuilder.control(null),
+      idCzlonekZespol: this.formBuilder.control(null),
+      kierunekStudiow: this.formBuilder.control(null),
+      nrTelefonu: this.formBuilder.control(null),
+      mail: this.formBuilder.control(null),
+      nazwisko: this.formBuilder.control(null),
+      imie: this.formBuilder.control(null),
+      uczelnia: this.formBuilder.control(null),
+      wydzial: this.formBuilder.control(null),
+    });
+
+  }
+
+  isValid(name: string): boolean {
+    const pole = this.formCzlonek.controls[name];
+    return pole.valid || !pole.touched;
+  }
+
+  onSubmit() {
+    const req: CzlonekRequest = new CzlonekRequest(this.formCzlonek.value.idPelnionejFunkcji, this.formCzlonek.value.idCzlonekZespol, 
+      this.formCzlonek.value.kierunekStudiow, this.formCzlonek.value.nrTelefonu, this.formCzlonek.value.mail, this.formCzlonek.value.nazwisko, this.formCzlonek.value.imie, 
+      this.formCzlonek.value.uczelnia, this.formCzlonek.value.wydzial);
+
+    // if(this.projekt != null) {
+    //   this.sekretarzService.put(this.projekt.idProjektu, req).subscribe({
+    //     next: (res) => {
+    //       this.router.navigateByUrl('');
+    //     }
+    //   });
+    // } else {
+    //   this.sekretarzService.post(req).subscribe({
+    //     next: (res) => {
+    //       this.router.navigateByUrl('');
+    //     }
+    //   });
+    // }
+  }
+}
+{
 
 }
